518aa252a49bb7577dc5523b5cb92475a49f3b80
Shannon Adams <88042360+SAdamss@users.noreply.github.com>
2022-12-03 09:37:24 +0000

    Add files via upload

---
+++ Broadway Conveniece Store/script.js
@@ -0,0 +1,11 @@
+
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+
